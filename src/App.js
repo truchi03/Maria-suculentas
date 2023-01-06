7da028d6b25494544377f0cd3ee0d75395629fc5
Alba Luz Saldarriaga <albast123@hotmail.com>
2023-01-05 23:22:03 -0500

    header,footer,logo,main,css styles

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Header } from './componentes/Header';
+import './App.css';
+import { Main } from './componentes/Main';
+import { Footer } from './componentes/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,9 @@
+import logo from '../assets/imagenes/plantas-maria.jpg'
+export function Header() {
+    return (
+        <header>
+            <img src={logo} alt='logo' id='logo' />
+             Maria Suculentas SAS | Inicio 
+        </header>
+    )
+}
